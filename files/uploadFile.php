534b5481a3cd62f7f63a5822871c95e0d3dd8d91
Anders <mail@andersandersson.net>
2020-03-20 10:47:58 +0100

    Add fileupload, add database connetion for fileupload

--- files/uploadFile.php
+++ files/uploadFile.php
@@ -5,9 +5,12 @@ include("../object/files.php");
 
 $fileHandler = new Files($databaseHandler);
 
+//print_r($_FILES);
+//die;
+
 if(!empty($_FILES['file_to_upload'])) {
 
-    echo $fileHandler->uploadFile($_FILES['file_to_upload']);
+    print_r($fileHandler->uploadFile($_FILES['file_to_upload']));
 
 } else {
     echo "Error! file_to_upload is missing!";
--- object/files.php
+++ object/files.php
@@ -6,6 +6,9 @@ include("../config/database_handler.php");
 class Files {
 
     private $database_handler;
+    private $upload_folder = "/opt/lampp/htdocs/api/uploads/";
+    private $public_upload_folder = "uploads/";
+
 
     public function __construct($databaseHandler) {
 
@@ -14,7 +17,29 @@ class Files {
     }
 
     private function addFileToDatabase($file_name) {
+        $query_string = "INSERT INTO files (file_path) VALUES(:file_path_IN)";
+        $statementHandler = $this->database_handler->prepare($query_string);
 
+        if($statementHandler !== false) {
+            
+            $statementHandler->bindParam(":file_path_IN", $file_name);
+            $statementHandler->execute();
+
+            $last_inserted_id = $this->database_handler->lastInsertId();
+
+            $query_string = "SELECT file_path FROM files WHERE id=:last_id_IN";
+            $statementHandler = $this->database_handler->prepare($query_string);
+            $statementHandler->bindParam(":last_id_IN", $last_inserted_id);
+            
+            $statementHandler->execute();
+
+            return $statementHandler->fetch()['file_path'];
+
+
+        } else {
+            return "ERROR";
+        }
+        
             // Lägg in sökvägen till filen i databasen.
 
     }
@@ -27,17 +52,33 @@ class Files {
 
     public function uploadFile($file) {
 
+        $returnObject = new stdClass();
+
         $file_original_name     = $file['name'];
         $file_type              = $file['type'];
         $file_tmp_name          = $file['tmp_name'];
         $file_errors            = $file['error'];
         $file_size              = $file['size'];
 
+        $slizedFile = explode(".", $file_original_name);
+        $uniqFileName = md5($slizedFile[0].uniqid('', true).time()) . "." . $slizedFile[1];
 
 
+        $targetFile = $this->upload_folder . $uniqFileName;
+        $isUploaded = move_uploaded_file($file_tmp_name, $targetFile);
+
+        if($isUploaded) {
+
+            $returnObject->filePath = $this->addFileToDatabase($uniqFileName);
+
+        } else {
+            $returnObject->message = "ERROR";
+        }
+
+        
 
 
-        return $file; // Returnera sökvägen.
+        return json_encode($returnObject); // Returnera sökvägen.
 
     }
 
